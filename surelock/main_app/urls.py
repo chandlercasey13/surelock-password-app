e5b351fa3d5263feeea486bd4f948544d3b50964
jcao91 <jcao91@yahoo.com>
2024-08-01 14:50:06 -0500

    create delete path

--- surelock/main_app/urls.py
+++ surelock/main_app/urls.py
@@ -5,5 +5,14 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("passwords/", views.password_index, name="password-index"),
     path("passwords/<int:password_id>/", views.password_detail, name="password-detail"),
-    path("password/<int:pk>/update/", views.PasswordUpdate.as_view(), name="password-update"),
+    path(
+        "password/<int:pk>/update/",
+        views.PasswordUpdate.as_view(),
+        name="password-update",
+    ),
+    path(
+        "passwords/<int:pk>/delete/",
+        views.PasswordDelete.as_view(),
+        name="password-delete",
+    ),
 ]
