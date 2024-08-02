9c93e5463e6ae251ef344e983a793db2a9f3b5a0
Chandler Casey <chandlercasey13@gmail.com>
2024-08-02 10:12:36 -0500

    Change Login Model remove defaults

--- surelock/main_app/models.py
+++ surelock/main_app/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Login(models.Model):
-    appname = models.CharField(default='No App Name', max_length=100)
-    username =  models.CharField(default = 'No Username', max_length=100)
-    password =  models.CharField(default = 'No Password', max_length=100)
-    note = models.TextField(default= 'Nothing Here', max_length=250)
+    appname = models.CharField(max_length=100)
+    username =  models.CharField(max_length=100)
+    password =  models.CharField(max_length=100)
+    note = models.TextField(max_length=250)
 
     def __str__(self):
         return self.appname    
