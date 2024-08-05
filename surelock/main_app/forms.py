9700b2fd83ac6d0e66d7de93a8b72e70fbc866a1
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-08-04 21:20:37 -0500

    update model, forms.py and post request

--- surelock/main_app/forms.py
+++ surelock/main_app/forms.py
@@ -2,9 +2,7 @@ from django import forms
 from .models import Login
 
 
-
-
 class LoginForm(forms.ModelForm):
     class Meta:
         model = Login
-        fields = '__all__'  # or specify the fields you want
+        fields = ["appname", "username", "password", "note"]
