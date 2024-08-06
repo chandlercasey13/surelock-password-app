7cbc0e2395dc55c809b68c9580fbfa046b505db2
Chandler Casey <chandlercasey13@gmail.com>
2024-08-06 11:14:49 -0500

    merged changes

--- surelock/main_app/forms.py
+++ surelock/main_app/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from .models import Login
+from django.contrib.auth.hashers import make_password
 
 
 class LoginForm(forms.ModelForm):
     class Meta:
         model = Login
         fields = ["appname", "username", "password", "note"]
+
+        def save(self):
+            logininfo = super().save()  
+            logininfo.password = make_password(self.cleaned_data['password1'], hasher='main_app.hashers.MyBCryptSHA256PasswordHasher')  # Set the password manually with custom hasher
+            
+            logininfo.save()  
+            return logininfo
