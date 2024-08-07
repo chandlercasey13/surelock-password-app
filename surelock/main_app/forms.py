b7a486418be6eac09a342662e403fff0d54b4cdf
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-08-07 16:34:18 -0500

    remove unneeded paths in urls.py

--- surelock/main_app/forms.py
+++ surelock/main_app/forms.py
@@ -9,8 +9,11 @@ class LoginForm(forms.ModelForm):
         fields = ["appname", "username", "password", "note"]
 
         def save(self):
-            logininfo = super().save()  
-            logininfo.password = make_password(self.cleaned_data['password1'], hasher='main_app.hashers.MyBCryptSHA256PasswordHasher')  # Set the password manually with custom hasher
-            
-            logininfo.save()  
+            logininfo = super().save()
+            logininfo.password = make_password(
+                self.cleaned_data["password1"],
+                hasher="main_app.hashers.MyBCryptSHA256PasswordHasher",
+            )  # Set the password manually with custom hasher
+
+            logininfo.save()
             return logininfo
