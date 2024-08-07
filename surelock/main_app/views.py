0dcbbe277b6e2231791cd4945dc47c02c78a0a13
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-08-07 16:41:40 -0500

    views.py cleaned up

--- surelock/main_app/views.py
+++ surelock/main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView
 
 from django.views import View
 
@@ -12,8 +12,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Login  # imports Login model from models.py
-from django.urls import reverse_lazy
+from .models import Login
 
 
 class Home(LoginView):
@@ -24,12 +23,8 @@ class PassCreate(CreateView):
     model = Login
     fields = ["username", "password", "note"]
 
-    # This inherited method is called when a
-    # valid login/password form is being submitted
     def form_valid(self, form):
-        # Assign the logged in user (self.request.user)
-        form.instance.user = self.request.user  # form.instance is the cat
-        # Let the CreateView do its job as usual
+        form.instance.user = self.request.user
         return super().form_valid(form)
 
 
@@ -49,27 +44,16 @@ def password_detail(request, password_id):
 def signup(request):
     error_message = ""
     if request.method == "POST":
-        # This is how to create a 'user' form object
-        # that includes the data from the browser
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            # This will add the user to the database
             user = form.save()
-            # This is how we log a user in
             login(request, user)
             return redirect("password-index")
         else:
             error_message = "Invalid Signup - Try again"
-    # A bad POST or a GET request, so render signup.html with an empty form
     form = UserCreationForm()
     context = {"form": form, "error_message": error_message}
     return render(request, "signup.html", context)
-    # Same as:
-    # return render(
-    #     request,
-    #     'signup.html',
-    #     {'form': form, 'error_message': error_message}
-    # )
 
 
 class PassCreate(LoginRequiredMixin, CreateView):
@@ -98,9 +82,7 @@ class PasswordUpdate(LoginRequiredMixin, UpdateView):
 
         if password_id:
             login_instance = get_object_or_404(self.model, id=password_id)
-            form = self.form_class(
-                instance=login_instance
-            )  # Populate the form with the specific Login object
+            form = self.form_class(instance=login_instance)
         else:
             form = self.form_class()
         return render(
@@ -108,11 +90,6 @@ class PasswordUpdate(LoginRequiredMixin, UpdateView):
             "passwords/index.html",
             {"passwords": passwords, "updateform": form, "password_id": password_id},
         )
-
-
-# class PasswordDelete(DeleteView):
-#     model = Login
-#     success_url = "/passwords/"
 
 
 class CrudView(LoginRequiredMixin, View):
@@ -129,9 +106,7 @@ class CrudView(LoginRequiredMixin, View):
 
         if password_id:
             login_instance = get_object_or_404(self.model, id=password_id)
-            form = self.form_class(
-                instance=login_instance
-            )  # Populate the form with the specific Login object
+            form = self.form_class(instance=login_instance)
         else:
             form = self.form_class()
         return render(
@@ -144,11 +119,11 @@ class CrudView(LoginRequiredMixin, View):
         form = self.form_class(request.POST)
         if form.is_valid():
             login_instance = form.save(commit=False)
-            login_instance.user = request.user  # Set the logged-in user
+            login_instance.user = request.user
             login_instance.save()
             return redirect(self.success_url)
         else:
-            print(form.errors)  # Print form errors for debugging
+            print(form.errors)
             passwords = Login.objects.filter(user=request.user)
             return render(
                 request, self.template_name, {"passwords": passwords, "form": form}
@@ -179,7 +154,7 @@ def simple_password_create(request):
             login_instance = form.save(commit=False)
             login_instance.user = request.user
             login_instance.save()
-            return redirect("/passwords/")  # Replace with your correct redirect URL
+            return redirect("/passwords/")
     else:
         form = LoginForm()
     return render(request, "passwords/index.html", {"form": form})
