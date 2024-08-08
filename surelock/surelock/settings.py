c2c23e416ae5682c19b6e803e31dd6b5f420d689
padcoding1 <padcoding@gmail.com>
2024-08-08 06:55:34 -0600

    Fix signup button and some styling on signup page

--- surelock/surelock/settings.py
+++ surelock/surelock/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 
 from main_app.hashers import MyBCryptSHA256PasswordHasher
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
